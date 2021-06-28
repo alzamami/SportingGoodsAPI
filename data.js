3c7bd083228fab0d6d58639e2c528b2562def0b8
Mohammad Alzamami <alzamami981@gmail.com>
2021-06-28 20:30:31 +0300

    finished feature 1

---
+++ data.js
@@ -0,0 +1,40 @@
+const products = [
+  {
+    id: 1,
+    name: "UEFA Ball",
+    slug: "uefa-ball",
+    price: 12,
+    image:
+      "https://www.rebelsport.com.au/dw/image/v2/BBRV_PRD/on/demandware.static/-/Sites-srg-internal-master-catalog/default/dwc622546c/images/59573601/Rebel_59573601_hi-res.jpg?sw=1000&sh=1000&sm=fit",
+    description: "The UEFA 2021 finals ball",
+  },
+  {
+    id: 2,
+    name: "Jumping Rope",
+    slug: "jumping-rope",
+    price: 5,
+    image:
+      "https://www.dragonsports.eu/333581-verylarge_default/nylon-jumping-rope-grip-foam-adi1102-adidas.jpg",
+    description: "not like any other Jumping rope",
+  },
+  {
+    id: 3,
+    name: "Boxing Gloves",
+    slug: "boxing-gloves",
+    price: 11,
+    image:
+      "https://hayabusa-us.imgix.net/products/HayabusaCaptainAmericaBoxingGloves_MainImg_22f6b807-1e9c-4360-a44e-017d1ed48630.png?fm=pjpg&q=80&w=1200&h=1200&fit=fill&bg=F4F4F4&auto=compress,format",
+    description: "The Gloves Floyd Mayweather used in his last Official game",
+  },
+  {
+    id: 4,
+    name: "Boxing Gloves",
+    slug: "boxing-gloves1",
+    price: 11,
+    image:
+      "https://hayabusa-us.imgix.net/products/HayabusaCaptainAmericaBoxingGloves_MainImg_22f6b807-1e9c-4360-a44e-017d1ed48630.png?fm=pjpg&q=80&w=1200&h=1200&fit=fill&bg=F4F4F4&auto=compress,format",
+    description: "The Gloves Floyd Mayweather used in his last Official game",
+  },
+];
+
+module.exports = products;
